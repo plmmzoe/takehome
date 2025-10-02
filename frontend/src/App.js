e84c7ab93f0e9639ab1362b33105ba084eb58711
Zoe Zhang <zoez@Zoes-MacBook-Air.local>
2025-10-02 11:29:28 -0700

    TH-4 update UI to use update api and use user input to query data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,35 +2,43 @@ import { Select, MultiSelect } from '@mantine/core';
 import { useEffect, useState } from 'react';
 
 function App() {
-  const [category, setCategory] = useState();
-  const [tag, setTag] = useState();
-  const [desc, setDesc] = useState('');
-
+  const [categoryFilter, setCategoryFilter] = useState('');
+  const [tagFilter, setTagFilter] = useState([]);
+  const [descFilter, setDescFilter] = useState('');
   const [data, setData] = useState([]);
 
-  const [error, setError] = useState();
+  const [categories, setCategories] = useState([]);
+  const [tags, setTags] = useState([]);
+  const [descs, setDescs] = useState('');
 
   useEffect(() => {
     fetchData();
+    fetch('/api/categories/')
+      .then((res) => res.json())
+      .then((res) => setCategories(res.map((e) => e.name)));
+    fetch('/api/tags/')
+      .then((res) => res.json())
+      .then((res) => setTags(res.map((e) => e.name)));
   }, []);
 
+
+  useEffect(() => {
+    fetchData();
+  }, [categoryFilter, tagFilter, descFilter]);
+
   const fetchData = async () => {
-    try {
-      //get data
-      const response = await fetch('/api/listings/');
-      if (!response.ok) {
-        throw new Error(`Error: ${response.status}`);
-      }
-      const result = await response.json();
-      setData(result);
+    //get data
+    const params = new URLSearchParams();
+    if (descFilter) params.append('desc', descFilter);
+    if (categoryFilter) params.append('category', categoryFilter);
+    tagFilter.forEach((t) => params.append('tags', t));
 
-      // get cetegory
-
-      // get tag
-
-    } catch (err) {
-      setError(String(err));
+    const response = await fetch(`/api/listings/?${params.toString()}`);
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
     }
+    const result = await response.json();
+    setData(result);
   };
 
   return (
@@ -42,10 +50,16 @@ function App() {
     }}>
       <div>
         <Select
-          value={category}
-          onChange={setCategory}
+          value={categoryFilter}
+          onChange={setCategoryFilter}
           label={'Category'}
-          data={['']}
+          data={categories}
+        />
+        <MultiSelect
+          value={tagFilter}
+          onChange={setTagFilter}
+          label={'Tags'}
+          data={tags}
         />
       </div>
     </div>
