33e45ba76cc92ee197b75f9b7d04eebf86328b9c
Zoe Zhang <zoez@Zoes-MacBook-Air.local>
2025-10-01 23:39:51 -0700

    TH-1 update model to define foreign key for category

--- backend/listings/admin.py
+++ backend/listings/admin.py
@@ -1,8 +1,28 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Tag, Category
 
-class ListingAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description')
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'get_category', 'get_tags') # def tuple
+ 
+    def get_category(self, obj):
+        return obj.category.name
+    
+    def get_tags(self, obj):
+        return ', '.join(e.name for e in obj.tags.all())
+    
+    get_category.short_description = 'Category'
+    get_tags.short_description = 'Tags'
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    # TODO: Tag operation pending
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    # TODO: Category operations pending
 
 # Register your models here.
-admin.site.register(Product, ListingAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Category, CategoryAdmin)
--- backend/listings/models.py
+++ backend/listings/models.py
@@ -3,11 +3,31 @@ from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 
-class Product(models.Model):
-    name = models.CharField()
-    description = models.TextField()
-    category = models.TextField()
-    tags = ArrayField(models.TextField())
+# Define Category Model
+class Category(models.Model):
+    name = models.CharField(max_length=128)
 
-    def _str_(self):
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=128)
+    
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+    category = models.ForeignKey(
+        Category, 
+        on_delete = models.CASCADE, # delete all product under the category when delete category
+        related_name = 'products'
+    )
+    tags = models.ManyToManyField(
+        Tag, 
+        related_name = 'products'
+    )
+
+    def __str__(self):
         return self.name
