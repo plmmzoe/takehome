83cae452d292ff64c62aa183f4bc23e54836be12
Zoe Zhang <zoez@Zoes-MacBook-Air.local>
2025-10-02 03:43:37 -0700

    TH-4 add mantine fe lib for filter ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { Select, MultiSelect } from '@mantine/core';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [category, setCategory] = useState();
+  const [tag, setTag] = useState();
+  const [desc, setDesc] = useState('');
+
+  const [data, setData] = useState([]);
+
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      //get data
+      const response = await fetch('/api/listings/');
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+      const result = await response.json();
+      setData(result);
+
+      // get cetegory
+
+      // get tag
+
+    } catch (err) {
+      setError(String(err));
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      padding: '10px',
+      display: 'flex',
+      flexDirection: 'row',
+      width: '300px',
+    }}>
+      <div>
+        <Select
+          value={category}
+          onChange={setCategory}
+          label={'Category'}
+          data={['']}
+        />
+      </div>
     </div>
+
   );
 }
 
